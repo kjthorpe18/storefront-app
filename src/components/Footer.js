a1a679b6576b04ccf5ecf0028d8d6679f0fc38ca
kjthorpe18 <kjt29@pitt.edu>
2022-01-22 12:36:50 -0500

    Footer starter and yarn format script

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div id="footer">
+        <div id="profile">
+          <p>TEST FOOTER</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Nav from "./Nav";
+import Footer from "./Footer";
 
 class Layout extends Component {
   render() {
@@ -7,6 +8,7 @@ class Layout extends Component {
       <div>
         <Nav />
         <div className="content">{this.props.children}</div>
+        <Footer />
       </div>
     );
   }
