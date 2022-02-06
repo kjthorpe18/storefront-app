3c11920ba233d5c3cf151aa6726cac83dce1c5d2
kjthorpe18 <kjt29@pitt.edu>
2022-02-06 08:45:44 -0500

    Move routing to App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import * as React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Layout from "./components/Layout";
+import CreateAccount from "./views/CreateAccount";
+import Login from "./views/Login";
+import About from "./views/About";
+import CreateProduct from "./views/CreateProduct";
+import Shop from "./views/Shop";
 
 function App() {
-  return <Layout />;
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="about" element={<About />} />
+          <Route path="create-product" element={<CreateProduct />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="create-account" element={<CreateAccount />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./static/index.css";
 
 import App from "./App";
-import CreateAccount from "./views/CreateAccount";
-import Login from "./views/Login";
-import About from "./views/About";
-import CreateProduct from "./views/CreateProduct";
-import Shop from "./views/Shop";
+
+// function requireAuth(nextState, replace, next) {
+//   if (!authenticated) {
+//     replace({
+//       pathname: "/login",
+//       state: {nextPathname: nextState.location.pathname}
+//     });
+//   }
+//   next();
+// }
+
+// onEnter={requireAuth}
+
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="create-account" element={<CreateAccount />} />
-        <Route path="login" element={<Login />} />
-        <Route path="about" element={<About />} />
-        <Route path="create-product" element={<CreateProduct />} />
-        <Route path="shop" element={<Shop />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
+  <App />,
   document.getElementById("root")
 );
