c956085c62616b9f52f265fa152ff86d70c77647
kjthorpe18 <kjt29@pitt.edu>
2022-02-12 10:39:51 -0500

    Footer updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,49 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Footer extends Component {
   render() {
     return (
       <div id="footer">
-        <p>TEST FOOTER</p>
+        <div className="footer-column">
+          <h4 className="footer-column-header">Help</h4>
+          <ul className="footer-list">
+            <li>
+              <Link to="/faq">FAQ</Link>
+            </li>
+            <li>
+              <Link to="/orders">Orders</Link>
+            </li>
+            <li>
+              <Link to="/returns">Returns</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-column">
+          <h4 className="footer-column-header">Account</h4>
+          <ul className="footer-list">
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/create-account">Register</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-column">
+          <h4 className="footer-column-header">Pages</h4>
+          <ul className="footer-list">
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/shop">Shop</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
