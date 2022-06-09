cdb733cc1e61d631ae765bcaae9ec3a5bc9fbbb4
kjthorpe18 <kjt29@pitt.edu>
2022-06-09 11:29:01 -0400

    Reorganize API GW resources and add all resources for API

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -2,9 +2,46 @@ resource "aws_api_gateway_rest_api" "storefront_rest_api" {
   name = "storefront-rest-api"
 }
 
+# stage and deployment resources
+resource "aws_api_gateway_deployment" "test_deployment" {
+  rest_api_id = local.rest_api_id
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.storefront_rest_api.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "test_stage" {
+  deployment_id = aws_api_gateway_deployment.test_deployment.id
+  rest_api_id   = local.rest_api_id
+  stage_name    = "test"
+}
+
+locals {
+  rest_api_id = aws_api_gateway_rest_api.storefront_rest_api.id
+}
+
+# ------------------------------------------------------------
+
 resource "aws_api_gateway_resource" "cart_resource" {
   parent_id   = aws_api_gateway_rest_api.storefront_rest_api.root_resource_id
   path_part   = "cart"
+  rest_api_id = local.rest_api_id
+}
+
+resource "aws_api_gateway_resource" "add_resource" {
+  parent_id   = aws_api_gateway_resource.cart_resource.id
+  path_part   = "add"
+  rest_api_id = local.rest_api_id
+}
+
+resource "aws_api_gateway_resource" "checkout_resource" {
+  parent_id   = aws_api_gateway_resource.cart_resource.id
+  path_part   = "checkout"
   rest_api_id = local.rest_api_id
 }
 
@@ -39,25 +76,20 @@ resource "aws_lambda_permission" "apigw_lambda" {
   source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${local.rest_api_id}/*/${aws_api_gateway_method.get_cart_method.http_method}${aws_api_gateway_resource.cart_resource.path}"
 }
 
-# stage and deployment resources
-resource "aws_api_gateway_deployment" "test_deployment" {
+resource "aws_api_gateway_resource" "products_resource" {
+  parent_id   = aws_api_gateway_rest_api.storefront_rest_api.root_resource_id
+  path_part   = "products"
   rest_api_id = local.rest_api_id
-
-  triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.storefront_rest_api.body))
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
-resource "aws_api_gateway_stage" "test_stage" {
-  deployment_id = aws_api_gateway_deployment.test_deployment.id
-  rest_api_id   = local.rest_api_id
-  stage_name    = "test"
+resource "aws_api_gateway_resource" "users_resource" {
+  parent_id   = aws_api_gateway_rest_api.storefront_rest_api.root_resource_id
+  path_part   = "users"
+  rest_api_id = local.rest_api_id
 }
 
-locals {
-  rest_api_id = aws_api_gateway_rest_api.storefront_rest_api.id
+resource "aws_api_gateway_resource" "login_resource" {
+  parent_id   = aws_api_gateway_resource.users_resource.id
+  path_part   = "login"
+  rest_api_id = local.rest_api_id
 }
