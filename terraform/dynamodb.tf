49220128464351ae6620fce274b7c8892632fe1c
kjthorpe18 <kjt29@pitt.edu>
2022-06-14 19:20:38 -0400

    Track dynamo tables

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -2,4 +2,37 @@ resource "aws_dynamodb_table" "users_dynamodb_table" {
   name           = "users"
   read_capacity  = 1
   write_capacity = 1
+
+  hash_key       = "email"
+
+   attribute {
+    name = "email"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "products_dynamodb_table" {
+  name           = "storefront-products"
+  read_capacity  = 1
+  write_capacity = 1
+
+  hash_key       = "productUUID"
+
+  attribute {
+    name = "productUUID"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "categories_dynamodb_table" {
+  name           = "storefront-categories"
+  read_capacity  = 1
+  write_capacity = 1
+
+  hash_key       = "category"
+
+  attribute {
+    name = "category"
+    type = "S"
+  }
 }
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -11,7 +11,7 @@ resource "aws_iam_role" "lambda_role" {
       {
         Action = "sts:AssumeRole"
         Effect = "Allow"
-        Sid    = ""
+        Sid    = "LambdaAssumeRole"
         Principal = {
           Service = "lambda.amazonaws.com"
         },
